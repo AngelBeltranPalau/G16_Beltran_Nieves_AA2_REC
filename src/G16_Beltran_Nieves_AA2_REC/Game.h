3f4a26151176fc4ca23d4d02642019e85558b9e6
Àngel Beltrán <angelbeltranpalau@hotmail.com>
2018-01-21 12:15:11 +0100

    Game.h

--- src/G16_Beltran_Nieves_AA2_REC/Game.h
+++ src/G16_Beltran_Nieves_AA2_REC/Game.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "Scene.h"
+
+//Enum con las tres escenas del juego
+enum estadoJuego { LEVEL, MENU, RANKING};
+
 class Game
 {
 public:
 	Game();
 	~Game();
-};
 
+private:
+
+	//Esta variable nos servirá para sabber en que escena estamos actualmente
+	estadoJuego estadoActual;
+
+	//Este puntero apuntará a la instancia de la escena actual
+	Scene *escenaActual;
+
+};
